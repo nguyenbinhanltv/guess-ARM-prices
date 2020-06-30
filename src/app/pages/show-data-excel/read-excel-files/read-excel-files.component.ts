cd9b6f63c2cb03da111126f8a0a320f8c181c5fa
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-06-30 08:44:30 +0700

    upload files ng zorro

--- src/app/pages/show-data-excel/read-excel-files/read-excel-files.component.ts
+++ src/app/pages/show-data-excel/read-excel-files/read-excel-files.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { DataFilesService } from 'src/app/services/data-files.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-read-excel-files',
@@ -8,18 +10,21 @@ import { DataFilesService } from 'src/app/services/data-files.service';
   styleUrls: ['./read-excel-files.component.scss'],
 })
 export class ReadExcelFilesComponent implements OnInit {
-  constructor(private dataFilesService: DataFilesService) {}
+  constructor(
+    private dataFilesService: DataFilesService,
+    private msg: NzMessageService
+    ) {}
 
   ngOnInit(): void {}
 
-  onFileChange(event: any) {
-    /* wire up file reader */
-    const target: DataTransfer = event.target as DataTransfer;
-    if (target.files.length !== 1) {
+  onFileChange(file: any) {
+    console.log(file);
+
+    if (file.length !== 1) {
       throw new Error('Cannot use multiple files');
     }
     const reader: FileReader = new FileReader();
-    reader.readAsBinaryString(target.files[0]);
+    reader.readAsBinaryString(file);
     reader.onload = (e: any) => {
       /* create workbook */
       const binarystr: string = e.target.result;
@@ -34,5 +39,17 @@ export class ReadExcelFilesComponent implements OnInit {
       this.dataFilesService.data = data;
       console.log(data);
     };
+  }
+
+  handleChange(info: NzUploadChangeParam): void {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      this.msg.success(`${info.file.name} file uploaded successfully`);
+      this.onFileChange(info.file);
+    } else if (info.file.status === 'error') {
+      this.msg.error(`${info.file.name} file upload failed.`);
+    }
   }
 }
--- src/app/pages/show-data-excel/show-data-excel.module.ts
+++ src/app/pages/show-data-excel/show-data-excel.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ShowDataExcelRoutingModule } from './show-data-excel-routing.module';
 import { ShowDataExcelComponent } from './show-data-excel.component';
 import { ReadExcelFilesComponent } from './read-excel-files/read-excel-files.component';
+import { NgZorroAntdModule } from 'src/app/ng-zorro-antd.module';
 
 
 @NgModule({
   declarations: [ShowDataExcelComponent, ReadExcelFilesComponent],
   imports: [
     CommonModule,
-    ShowDataExcelRoutingModule
+    ShowDataExcelRoutingModule,
+    NgZorroAntdModule
   ]
 })
 export class ShowDataExcelModule { }
